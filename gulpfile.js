fcbaa9b6f461851939a81a5a797856433eccfb29
Sławomir Jopek <sjopek86@gmail.com>
2017-07-10 14:30:41 +0200

    gulp clear dist, customizing bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
+var wiredep = require('wiredep');
+var del = require('del');
+
+gulp.task('build', ['clean', 'html']);
 
 gulp.task('html', ['styles'], function() {
     var injectStyles = gulp.src('dist/main.css');
@@ -14,7 +18,26 @@ gulp.task('html', ['styles'], function() {
 });
 
 gulp.task('styles', function() {
+    var injectGlobalStyles = gulp.src('src/styles/global/*.scss');
+
+    var options = {
+        transform: createImport,
+        starttag: '// inject:global',
+        endtag: '// endinject',
+        addRootSlash: false
+    };
+
     return gulp.src('src/main.scss')
+        .pipe(wiredep.stream())
+        .pipe(inject(injectGlobalStyles, options))
         .pipe(sass())
         .pipe(gulp.dest('dist'))
 });
+
+gulp.task('clean', function() {
+    del('dist');
+});
+
+function createImport(filepath) {
+    return '@import "' + filepath + '";';
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,19 @@
-install:
+**install:**
 - npm install
 - bower install
+- gulp init
 
-html, styles init:
+**styles + html build:**
+- gulp build
+
+**styles build:**
+- gulp styles
+
+**html build:**
 - gulp html
+
+**remove dist:**
+- gulp clear
+
+**bootstrap customize:**
+_src/styles/global/*.scss_
