5bd999f0912d5a55a15a043334bf0ec422b81871
Sławomir Jopek <sjopek86@gmail.com>
2017-07-10 17:07:52 +0200

    gulp inject app styles, inject bower vendors

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,74 @@ var sass = require('gulp-sass');
 var inject = require('gulp-inject');
 var wiredep = require('wiredep');
 var del = require('del');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
 
-gulp.task('build', ['clean', 'html']);
+var CONFIG = {
+    srcName: 'src',
+    distName: 'dist',
+    srcStyleDir: 'src/styles/',
+    distStyleDir: 'dist/styles/'
+};
 
-gulp.task('html', ['styles'], function() {
-    var injectStyles = gulp.src('dist/main.css');
-
-    return gulp.src('src/index.html')
-        .pipe(inject(injectStyles, {
-            addRootSlash: false,
-            ignorePath: ['src', 'dist']
-        }))
-        .pipe(gulp.dest('dist'))
+gulp.task('clean', function() {
+    del(CONFIG.distName);
 });
 
 gulp.task('styles', function() {
-    var injectGlobalStyles = gulp.src('src/styles/global/*.scss');
+    var appStyles = gulp.src(CONFIG.srcStyleDir + '*.scss', {read: false});
+    var globalStyles = gulp.src(CONFIG.srcStyleDir + 'global/*.scss', {read: false});
 
     var injectOptions = {
-        transform: createImport,
-        starttag: '// inject:global',
-        endtag: '// endinject',
-        addRootSlash: false
-    };
-
-    var sassCompileOptions = {
-        outputStyle: 'compressed'
+        global: {
+            transform: createImport,
+            starttag: '// inject:global',
+            endtag: '// endinject',
+            addRootSlash: false
+        },
+        app: {
+            transform: createImport,
+            starttag: '// inject:app',
+            endtag: '// endinject',
+            addRootSlash: false
+        }
     };
 
     return gulp.src('src/main.scss')
         .pipe(wiredep.stream())
-        .pipe(inject(injectGlobalStyles, injectOptions))
-        .pipe(sass(sassCompileOptions))
-        .pipe(gulp.dest('dist'))
+        .pipe(inject(globalStyles, injectOptions.global))
+        .pipe(inject(appStyles, injectOptions.app))
+        .pipe(sass())
+        .pipe(csso())
+        .pipe(gulp.dest(CONFIG.distStyleDir))
 });
 
-gulp.task('clean', function() {
-    del('dist');
+gulp.task('vendors', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(filter('**/*.css'))
+        .pipe(concat('vendor.css'))
+        .pipe(csso())
+        .pipe(gulp.dest(CONFIG.distStyleDir))
 });
 
-function createImport(filepath) {
-    return '@import "' + filepath + '";';
+gulp.task('build', ['clean', 'vendors', 'styles'], function() {
+    var files = gulp.src([
+        CONFIG.distStyleDir + 'vendors.css',
+        CONFIG.distStyleDir + 'main.css'
+    ]);
+
+    var injectOptions = {
+        addRootSlash: false,
+        ignorePath: [CONFIG.srcName, CONFIG.distName]
+    };
+
+    return gulp.src(CONFIG.srcName + '/index.html')
+        .pipe(inject(files, injectOptions))
+        .pipe(gulp.dest(CONFIG.distName))
+});
+
+function createImport(path) {
+    return '@import "' + path + '";';
 }
--- readme.md
+++ readme.md
@@ -1,19 +1,10 @@
 **install:**
 - npm install
 - bower install
-- gulp init
-
-**styles + html build:**
 - gulp build
 
-**styles build:**
-- gulp styles
-
-**html build:**
-- gulp html
-
-**remove dist:**
-- gulp clear
+**build:**
+- gulp build
 
 **bootstrap customize:**
 _src/styles/global/*.scss_
